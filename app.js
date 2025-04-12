958fe1edac0ca0a5144ceb0b525f2b9043f22aab
sandhya106 <sandhyasinhaa45@gmail.com>
2025-04-12 23:10:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
     ctrl.found = [];
 
     ctrl.narrowDown = function () {
-      if (!ctrl.searchTerm) {
+      if (!ctrl.searchTerm.trim()) {
         ctrl.found = [];
         return;
       }
@@ -35,14 +35,11 @@
     service.getMatchedMenuItems = function (searchTerm) {
       return $http.get('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json')
         .then(function (response) {
-          var allItems = response.data.menu_items;
-          if (allItems && Array.isArray(allItems)) {
-            return allItems.filter(function (item) {
-              return item.description.toLowerCase().includes(searchTerm.toLowerCase());
-            });
-          } else {
-            return [];
-          }
+          var allItems = response.data.menu_items || [];
+          return allItems.filter(function (item) {
+            return item.description &&
+              item.description.toLowerCase().includes(searchTerm.toLowerCase());
+          });
         });
     };
   }
